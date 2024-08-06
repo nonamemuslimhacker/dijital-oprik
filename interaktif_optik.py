6f80bd6080d7d2142a4afdfe03788fcbcb2cabb3
nonamemuslimhacker <metrexon@gmail.com>
2024-08-06 14:50:32 +0300

    İnteraktif Opik

---
+++ interaktif_optik.py
@@ -0,0 +1,36 @@
+sinavadi = input("sınav ismi?")
+
+dersler = {}
+cevaplar = {}
+
+dersadi = input("ders adı?")
+sorusayisi = int(input(dersadi + " " + "soru sayısı"))
+while True:
+    dersler[dersadi] = sorusayisi
+
+    ss = int(input("cevapladığın sorunun numarası"))
+
+    if ss < 1 or ss > sorusayisi:
+        ss = int(input("lütfen geçerli aralıkta bir sayı giriniz"))
+
+
+    sh = input("cevapladığın sorunun şık ismi (a/b/c/d/e)")
+
+    if sh != "a" and sh != "b" and sh != "c" and sh != "d" and sh != "e":
+        sh = input("lütfen geçerli bir harf giriniz")
+
+    cevaplar[ss] = sh
+    dersler[dersadi] = cevaplar
+
+    so = input("eğer sorular bitti ise son yazın bitmedi yani hala cevaplamadığınız sorular var ise enter tuşuna bazınız")
+    if so != "son":
+        continue
+    print(dersler)
+    ds = input("sınavda başka ders varsa var yaz eğer yoksa son yaz ").lower()
+    if ds == "son":
+        break
+    elif ds != "son":
+        dersadi = input("eklemek veya geri dönmek istediğin dersin ismini yaz: ")
+        sorusayisi = int(input(dersadi + " " + "soru sayısı"))
+
+
